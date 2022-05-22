3b10f9daddd01cd856e7000c2f127f13d0f9d5c9
jackmleitch <jackmleitch@gmail.com>
2022-05-22 13:30:57 -0600

    feat: api to post requests to paraphrasing model

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from fastapi import Depends, FastAPI
+from pydantic import BaseModel
+
+from src.paraphrasing_model.onnx_T5_model import OnnxPipeline
+
+# load paraphrasing model
+print('Loading model & tokenizer...')
+paraphrasing_pipeline = OnnxPipeline(num_beams=5)
+print('Model & tokenizer loaded!')
+
+app = FastAPI()
+
+class ParaphrasingRequest(BaseModel):
+    text: str    
+
+class ParaphrasingResponse(BaseModel):
+    text: str
+
+@app.get("/")
+def get_root():
+    return "This is the RESTful API for EssayCompanion"
+
+@app.post("/predict", response_model=ParaphrasingResponse)
+async def predict(request: ParaphrasingRequest):
+    return ParaphrasingResponse(text=paraphrasing_pipeline(request.text))
