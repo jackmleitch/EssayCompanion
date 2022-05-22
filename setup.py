7cc35db7e5b7a83b43793ea723491a77f14c17c6
jackmleitch <jackmleitch@gmail.com>
2022-05-22 13:29:52 -0600

    build: added setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="EssayCompanion",
+    packages=find_packages(),
+    version="0.1.0",
+    description="A tool to paraphrase, summarize, and extract keywords from text.",
+    author="Jack Leitch",
+    license="MIT",
+)
